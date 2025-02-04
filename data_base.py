20b7f01bc8799fef1389baa1a812d493bae0c315
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-02-04 18:47:37 -0300

    add app route

---
+++ data_base.py
@@ -0,0 +1,122 @@
+cars = [
+    {
+       'id': 1,
+       'brand': 'Fiat',
+       'model': 'Marea',
+       'year': 1999 
+    },
+    {
+        'id': 2,
+        'brand': 'Fiat',
+        'model': 'Uno',
+        'year': 1992 
+    },
+    {
+        'id': 3,
+        'brand': 'Ford',
+        'model': 'Escort',
+        'year': 1985 
+    },
+    {
+        'id': 4,
+        'brand': 'Chevrolet',
+        'model': 'Chevette',
+        'year': 1978 
+    },
+    {
+        'id': 5,
+        'brand': 'Car 5',
+        'model': 'Model 5',
+        'year': 1975
+    },
+    {
+        'id': 6,
+        'brand': 'Car 6',
+        'model': 'Model 6',
+        'year': 1976
+    },
+    {
+        'id': 7,
+        'brand': 'Car 7',
+        'model': 'Model 7',
+        'year': 1977
+    },
+    {
+        'id': 8,
+        'brand': 'Car 8',
+        'model': 'Model 8',
+        'year': 1978
+    },
+    {
+        'id': 9,
+        'brand': 'Car 9',
+        'model': 'Model 9',
+        'year': 1979
+    },
+    {
+        'id': 10,
+        'brand': 'Car 10',
+        'model': 'Model 10',
+        'year': 1980
+    },
+    {
+       'id': 11,
+       'brand': 'Car 11',
+       'model': 'Model 11',
+       'year': 1981 
+    },
+    {
+        'id': 12,
+        'brand': 'Car 12',
+        'model': 'Model 12',
+        'year': 1982 
+    },
+    {
+        'id': 13,
+        'brand': 'Car 13',
+        'model': 'Model 13',
+        'year': 1983 
+    },
+    {
+        'id': 14,
+        'brand': 'Car 14',
+        'model': 'Model 14',
+        'year': 1984 
+    },
+    {
+        'id': 15,
+        'brand': 'Car 15',
+        'model': 'Model 15',
+        'year': 1985
+    },
+    {
+        'id': 16,
+        'brand': 'Car 16',
+        'model': 'Model 16',
+        'year': 1986
+    },
+    {
+        'id': 17,
+        'brand': 'Car 17',
+        'model': 'Model 17',
+        'year': 1987
+    },
+    {
+        'id': 18,
+        'brand': 'Car 18',
+        'model': 'Model 18',
+        'year': 1988
+    },
+    {
+        'id': 19,
+        'brand': 'Car 19',
+        'model': 'Model 19',
+        'year': 1989
+    },
+    {
+        'id': 20,
+        'brand': 'Car 20',
+        'model': 'Model 20',
+        'year': 1990
+    },   
+]
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from flask import Flask 
+from flask import Flask, make_response, jsonify
+from data_base import cars
 
 app = Flask(__name__)
+
+@app.route('/cars', methods=['GET'])
+def get_cars():
+    return make_response(
+        jsonify(cars)
+    )
 
 app.run()
